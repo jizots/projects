510fc3d834012e0201afd59ed8ae14cecbfd1864
田中草太 <hotph@MacBook-Air-4.local>
2023-06-08 11:58:59 +0900

    delete some item

--- univ/regu/ft_putadrs.c
+++
@@ -1,7 +0,0 @@
-#include "printf.h"
-
-void	ft_putadrs(uintptr_t address)
-{
-	ft_putstr("0x");
-	ft_puthex(address, 0);
-}
--- univ/regu/ft_putchar.c
+++
@@ -1,8 +0,0 @@
-#include "printf.h"
-
-int	ft_putchar(const char c)
-{
-	if (write(1, &c, 1) == -1)
-		return (-1);
-	return (c);
-}
